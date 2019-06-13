5ea9840178763d932a9564095a5eee92f2b7242b
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-13 20:23:08 +0100

    refactor pokedex

--- pokedex-app/src/components/App.js
+++ pokedex-app/src/components/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from "react";
-import getData from "./requestAPI";
 
 class App extends Component {
-  state = {
-    data: []
-  };
-
-  componentDidMount() {
-    getData()
-        .then(data => this.setState(({
-          data: data.results
-      })))
-      .catch(error => console.log(error));
-  }
+  static defaultProps = [
+    { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+    { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+    { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+    { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+    { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+    { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+    { id: 94, name: "Genar", type: "poison", base_experience: 225 },
+    { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+  ];
 
   render() {
     return (
       <div>
         <h1>Pokedex</h1>
-        {this.state.data.map(item => console.log(item.name))}
       </div>
     );
   }
