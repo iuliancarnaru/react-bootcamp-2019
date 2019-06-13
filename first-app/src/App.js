9eea7989b340a98cd880c58a39c6a6bb1d839f54
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-06-13 16:10:12 +0100

    fruits v4

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
         return (
             <div>
                 <h1>Hello from React App.</h1>
-                
+
                 {console.log(`I would like some ${fruit}, please.`)}
-                {console.log(`Here you go: ${fruit}`)}
+                {console.log(`Here is your: ${fruit}`)}
                 {console.log(`Delicious! May I have another?`)}
-                {console.log(`I am sorry we have only ${removeFruit(fruits, fruit)} left.`)}
+                {console.log(`I am sorry we have only this fruits left: ${removeFruit(fruits, fruit)}.`)}
             </div>
         );
     }
