727f13d68b29130b3810900450452238561d6a29
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-06-13 13:58:27 +0100

    fruits v2

--- first-app/src/helpers.js
+++ first-app/src/helpers.js
@@ -1,8 +1,10 @@
 export const generateRandomFruit = array => array[Math.floor(Math.random() * array.length)];
 export const removeFruit = (array, fruit) => {
-    let index = array.indexOf(fruit)
-    if (index > -1) {
-        array.splice(index,1)
-    }
-    return array;
+    // let index = array.indexOf(fruit)
+    // if (index > -1) {
+    //     array.splice(index,1)
+    // }
+    // return array;
+
+    return array.filter(fruits => fruits !== fruit);
 }
