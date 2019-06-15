860edfb92df8683aa7d6d7dfb5c21abe18366215
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-15 13:25:36 +0100

    flip coin logic done v2

--- flip-coin-app/src/Coin.js
+++ flip-coin-app/src/Coin.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Image(props) {
+function Coin(props) {
   const head = `Md0AAOSw8AtbeDTC`;
   const tail = `jZwAAOSwh81beDS7`;
 
@@ -16,7 +16,7 @@ function Image(props) {
   );
 }
 
-export default Image;
+export default Coin;
 
 // https://i.ebayimg.com/images/g/Md0AAOSw8AtbeDTC/s-l1600.jpg - head
 // https://i.ebayimg.com/images/g/jZwAAOSwh81beDS7/s-l1600.jpg - tail
